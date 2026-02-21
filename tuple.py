40d7567950cde4d4e980e7448dcd998a8bd20179
Kruthika A <akruthika0306@gmail.com>
2026-02-21 21:47:41 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,7 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+print(thistuple[1])
+thistuple 
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+print(len(thistuple))
