503ebd75c17623895ef50e931fdc01d50ca523ba
Kruthika A <akruthika0306@gmail.com>
2026-02-23 21:47:28 +0530

    Add files via upload

--- password.py
+++ password.py
@@ -1,8 +1,10 @@
-password = input("Enter password: ")
+import secrets
+import string
 
-if len(password) < 8:
-    print("Weak: Password too short")
-elif password.isalpha() or password.isdigit():
-    print("Medium: Add numbers and symbols")
-else:
-    print("Strong password!")
+def generate_password(length=12):
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(alphabet) for _ in range(length))
+    return password
+
+new_password = generate_password()
+print(f"Generated Password: {new_password}")
