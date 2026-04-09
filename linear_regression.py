786c4e8e97dc53d3d3da7305448a1e86dde28787
Kruthika A <akruthika0306@gmail.com>
2026-04-09 22:08:47 +0530

    Create linear_regression.py

---
+++ linear_regression.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 6, 8, 10])
+
+model = LinearRegression()
+model.fit(X, y)
+
+pred = model.predict([[6]])
+
+print("Prediction for 6:", pred[0])
