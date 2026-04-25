446792ddd851278d16ff2dc375d7dee4f03e4a5c
Kruthika A <akruthika0306@gmail.com>
2026-04-25 23:11:33 +0530

    Remove unnecessary whitespace in mini_banking_system.py

--- mini_banking_system.py
+++ mini_banking_system.py
@@ -1,9 +1,7 @@
 balance = 1000
-
 while True:
     print("\n1.Deposit  2.Withdraw  3.Check  4.Exit")
     choice = input("Choose: ")
-
     if choice == "1":
         amt = int(input("Amount: "))
         balance += amt
