6747dd88d703ec29f7e2868e70771cad65208056
Kruthika A <akruthika0306@gmail.com>
2026-05-07 20:08:54 +0530

    Fix formatting in loan details output

--- Compute_Loan.py
+++ Compute_Loan.py
@@ -3,9 +3,8 @@ rate = float(input("Enter annual interest rate (%): "))
 time = int(input("Enter time in years: "))
 
 amount = principal * (1 + rate/100) ** time
-
-print("\n--- Loan Details ---")
+print("\n--- Loan Details -----")
 print("Loan Amount      :", principal)
 print("Interest Rate(%) :", rate)
 print("Time (years)     :", time)
-print("Final Amount to be Paid:", amount)
+print("Final Amt to be Paid:", amount)
